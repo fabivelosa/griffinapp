967614f334983cb6cf06df9d70b69b256e889aaf
fabivelosa <fabivelosa@gmail.com>
2021-02-10 17:57:28 +0000

    mavenApp to pipeline test

---
+++ prodigiesApp/src/test/java/prodigiesApp/AppTest.java
@@ -0,0 +1,13 @@
+package prodigiesApp;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+	@Test
+	public void test() {
+		AppMain myTest = new AppMain();
+		assertEquals("Test Passed!", myTest.test());
+	}
+}
