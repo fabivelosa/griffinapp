3621c2befde260fc57d49ca35453740c13636074
Wilmir <A00278899@student.ait.ie>
2021-02-15 23:14:00 +0000

    Revert "adds an empty switch statement to push a PMD violative code"
    This reverts commit b08c6e8d048163331741707776b4c14b4c192fc7.

--- prodigiesApp/src/main/java/prodigiesApp/AppMain.java
+++ prodigiesApp/src/main/java/prodigiesApp/AppMain.java
@@ -4,10 +4,6 @@ package prodigiesApp;
 public class AppMain {
     public static void main(String[] args) {
         System.out.println("Hello World 1!");
-        
-        switch("hello") {
-        
-        }
     }
     public String test() {
         return "Test Passed!";
