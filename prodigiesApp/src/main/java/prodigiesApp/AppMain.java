b08c6e8d048163331741707776b4c14b4c192fc7
Wilmir <A00278899@student.ait.ie>
2021-02-15 23:14:00 +0000

    adds an empty switch statement to push a PMD violative code

--- prodigiesApp/src/main/java/prodigiesApp/AppMain.java
+++ prodigiesApp/src/main/java/prodigiesApp/AppMain.java
@@ -4,6 +4,10 @@ package prodigiesApp;
 public class AppMain {
     public static void main(String[] args) {
         System.out.println("Hello World 1!");
+        
+        switch("hello") {
+        
+        }
     }
     public String test() {
         return "Test Passed!";
