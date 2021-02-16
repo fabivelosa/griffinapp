69303adcbba881e736c6eff2b7de59318262d93b
Peter Casey <A00278938@student.ait.ie>
2021-02-16 14:52:23 +0000

    Removed failed switch
    switch "wilmir" removed

--- prodigiesApp/src/main/java/prodigiesApp/AppMain.java
+++ prodigiesApp/src/main/java/prodigiesApp/AppMain.java
@@ -5,9 +5,11 @@ public class AppMain {
     public static void main(String[] args) {
         System.out.println("Hello World 1!");
         
-        switch("wilmir") {
+      
         
-        }
+     
+        
+        
     }
     public String test() {
         return "Test Passed!";
