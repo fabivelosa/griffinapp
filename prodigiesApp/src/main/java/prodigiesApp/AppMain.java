abcf395a001e29a1c1e50d888ffc0f5899a46091
Wilmir <A00278899@student.ait.ie>
2021-02-16 15:21:19 +0000

    Removes the sysout for current directory in AppMain

--- prodigiesApp/src/main/java/prodigiesApp/AppMain.java
+++ prodigiesApp/src/main/java/prodigiesApp/AppMain.java
@@ -4,9 +4,6 @@ package prodigiesApp;
 public class AppMain {
     public static void main(String[] args) {
         System.out.println("Hello World 1!");
-        
-        System.out.println(System.getProperty("user.dir"));
-        
     }
     
     public String test() {
