d89a30df0c1213d6917e1ad38e9d90bb116b3963
Wilmir <A00278899@student.ait.ie>
2021-02-16 15:09:32 +0000

    Adds the test datasets

--- prodigiesApp/src/main/java/prodigiesApp/AppMain.java
+++ prodigiesApp/src/main/java/prodigiesApp/AppMain.java
@@ -5,10 +5,10 @@ public class AppMain {
     public static void main(String[] args) {
         System.out.println("Hello World 1!");
         
-        switch("wilmir") {
+        System.out.println(System.getProperty("user.dir"));
         
-        }
     }
+    
     public String test() {
         return "Test Passed!";
     }
