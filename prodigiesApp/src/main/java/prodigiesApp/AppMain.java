8838b5137b8c6df54327186180686e79a8150db4
Monisha Mani <a00279057@student.ait.ie>
2021-02-18 15:44:25 +0000

    Changes for nial

--- prodigiesApp/src/main/java/prodigiesApp/AppMain.java
+++ prodigiesApp/src/main/java/prodigiesApp/AppMain.java
@@ -3,7 +3,7 @@ package prodigiesApp;
 
 public class AppMain {
     public static void main(String[] args) {
-        System.out.println("Hello Nial!!!");
+        System.out.println("Hello Nial!");
     }
     
     public String test() {
