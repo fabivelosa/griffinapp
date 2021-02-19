efa172d4bf366ebcf3ed191fac94402d45c40c91
Monisha Mani <a00279057@student.ait.ie>
2021-02-19 17:32:22 +0000

    unit tests for CallFailuyreDAO and SystemAdminManager

--- prodigiesApp/src/main/java/com/callFailures/entity/CallFailure.java
+++ prodigiesApp/src/main/java/com/callFailures/entity/CallFailure.java
@@ -15,74 +15,107 @@ public class CallFailure {
 	private int causeCode;
 	private int neVersion;
 	private long imsiID;
+	private long hier_3;
+    private long hier_32;
+	private long hier_321;
+	
+	
 	public Date getEventTime() {
 		return eventTime;
 	}
-	public void setEventTime(Date eventTime) {
+	final public void setEventTime(Date eventTime) {
 		this.eventTime = eventTime;
 	}
+	
 	public int getEventID() {
 		return eventID;
 	}
-	public void setEventID(int eventID) {
+	final public void setEventID(int eventID) {
 		this.eventID = eventID;
 	}
+	
 	public boolean isFailureClass() {
 		return failureClass;
 	}
-	public void setFailureClass(boolean failureClass) {
+	final public void setFailureClass(boolean failureClass) {
 		this.failureClass = failureClass;
 	}
+	
 	public int getUeType() {
 		return ueType;
 	}
-	public void setUeType(int ueType) {
+	final public void setUeType(int ueType) {
 		this.ueType = ueType;
 	}
+	
 	public int getMarketCode() {
 		return marketCode;
 	}
-	public void setMarketCode(int marketCode) {
+	final public void setMarketCode(int marketCode) {
 		this.marketCode = marketCode;
 	}
+	
 	public int getOperatorCode() {
 		return operatorCode;
 	}
-	public void setOperatorCode(int operatorCode) {
+	final public void setOperatorCode(int operatorCode) {
 		this.operatorCode = operatorCode;
 	}
+	
 	public int getCellID() {
 		return cellID;
 	}
-	public void setCellID(int cellID) {
+	final public void setCellID(int cellID) {
 		this.cellID = cellID;
 	}
+	
 	public int getDuration() {
 		return duration;
 	}
-	public void setDuration(int duration) {
+	final public void setDuration(int duration) {
 		this.duration = duration;
 	}
+	
 	public int getCauseCode() {
 		return causeCode;
 	}
-	public void setCauseCode(int causeCode) {
+	final public void setCauseCode(int causeCode) {
 		this.causeCode = causeCode;
 	}
+	
 	public int getNeVersion() {
 		return neVersion;
 	}
-	public void setNeVersion(int neVersion) {
+	final public void setNeVersion(int neVersion) {
 		this.neVersion = neVersion;
 	}
+	
 	public long getImsiID() {
 		return imsiID;
 	}
-	public void setImsiID(long imsiID) {
+	final public void setImsiID(long imsiID) {
 		this.imsiID = imsiID;
+	}
+	public long getHier_3() {
+		return hier_3;
+	}
+	final public void setHier_3(long hier_3) {
+		this.hier_3 = hier_3;
+	}
+	
+	public long getHier_32() {
+		return hier_32;
+	}
+	final public void setHier_32(long hier_32) {
+		this.hier_32 = hier_32;
+	}
+	
+	public long getHier_321() {
+		return hier_321;
+	}
+	final public void setHier_321(long hier_321) {
+		this.hier_321 = hier_321;
 	}
 	
 	
-	
-
 }
