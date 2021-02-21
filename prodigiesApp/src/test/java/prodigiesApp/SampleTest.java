98908644758028b86c407a1628ae2e1c35567022
Peter Casey <A00278938@student.ait.ie>
2021-02-21 20:00:20 +0000

    sample test

---
+++ prodigiesApp/src/test/java/prodigiesApp/SampleTest.java
@@ -0,0 +1,21 @@
+package prodigiesApp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SampleTest {
+
+	private String abadnamenoknbjf = "hi";
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testSuccess() {
+		assertEquals("hi", abadnamenoknbjf);
+	}
+	
+
+}
