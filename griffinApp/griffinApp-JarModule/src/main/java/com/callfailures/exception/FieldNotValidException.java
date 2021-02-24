572112c170b19a04b5d6d89d68a30f114d0fdfa9
Wilmir <A00278899@student.ait.ie>
2021-02-24 22:39:29 +0000

    Adds the FieldNotValidException

---
+++ griffinApp/griffinApp-JarModule/src/main/java/com/callfailures/exception/FieldNotValidException.java
@@ -0,0 +1,31 @@
+package com.callfailures.exception;
+
+public class FieldNotValidException extends RuntimeException{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8091254719370670655L;
+	
+	/**
+	 * The name of the invalid field
+	 */
+	private final String invalidFieldName;
+
+	public FieldNotValidException(final String invalidFieldName, final String message) {
+		super(message);
+		this.invalidFieldName = invalidFieldName;
+	}
+
+	public String getInvalidFieldName() {
+		return invalidFieldName;
+	}
+
+	@Override
+	public String toString() {
+		return "FieldNotValidException [invalidFieldName=" + invalidFieldName + "]";
+	}
+
+
+	
+	
+}
