b8c5a7a7db6a3432fa635ad31d63499a6c5e5c53
Wilmir <A00278899@student.ait.ie>
2021-02-27 17:56:54 +0000

    Refactored EventServiceImpl class

--- griffinApp/griffinApp-JarModule/src/main/java/com/callfailures/services/impl/EventServiceImpl.java
+++ griffinApp/griffinApp-JarModule/src/main/java/com/callfailures/services/impl/EventServiceImpl.java
@@ -35,41 +35,30 @@ public class EventServiceImpl implements EventService {
 	@Inject
 	ValidationService validationService;
 
-
 	@Override
 	public Events findById(final int id) {
-
 		return eventDAO.getEvent(id);
 	}
 
 	@Override
 	public void create(Events event) {
 		eventDAO.create(event);
-
 	}
 
 	@Override
 	public ParsingResponse<Events> read(File workbookFile) {
 		final ParsingResponse<Events> parsingResult = new ParsingResponse<>();
-		
 		try(Workbook workbook = new XSSFWorkbook(workbookFile);) {
-
 			Sheet sheet = workbook.getSheetAt(0);
 			Iterator<Row> rowIterator = sheet.rowIterator();
-
 			Row row = rowIterator.next();
-
 			int rowNumber = 0;
-
 			while (rowIterator.hasNext()) {
 				rowNumber++;
 				row = rowIterator.next();
-
 				try {
 					Events events = createEventObject(row);
-
 					eventDAO.create(events);
-
 					parsingResult.addValidObject(events);
 				}catch(FieldNotValidException e) {
 					parsingResult.addInvalidRow(new InvalidRow(rowNumber, e.getMessage()));
@@ -78,42 +67,34 @@ public class EventServiceImpl implements EventService {
 		} catch (IOException | InvalidFormatException e) {
 			e.printStackTrace();
 		}
-
 		return parsingResult;
 	}
 
 	
 	private Events createEventObject(Row row) {
 		Events events = new Events();
-
-		events.setDateTime(validationService.checkDate(row, 0));
-
-		events.setCellId(validationService.checkCellId(row, 6));
-
-		events.setDuration(validationService.checkDuration(row, 7));
-
-		events.setNeVersion(validationService.checkNEVersion(row, 9));
-
-		events.setImsi(validationService.checkIMSI(row, 10));
-
-		events.setHier3Id(validationService.checkhier3Id(row, 11));
-
-		events.setHier32Id(validationService.checkhier32Id(row, 12));
-
-		events.setHier321Id(validationService.checkhier321Id(row, 13));
-
-		events.setEventCause(validationService.checkExistingEventCause(row, 1, 8));
-
-		events.setFailureClass(validationService.checkExistingFailureClass(row, 2));
-
-		events.setUeType(validationService.checkExistingUserEquipmentType(row, 3));
-
-		events.setMarketOperator(validationService.checkExistingMarketOperator(row, 4,5));
-
-		System.out.println(events);
-
+		validateNonDatabaseDependentFields(row, events);
+		validateDatabaseDependendentFields(row, events);
 		validationService.validate(events);
 		return events;
+	}
+
+	private void validateDatabaseDependendentFields(Row row, Events events) {
+		events.setHier321Id(validationService.checkhier321Id(row, 13));
+		events.setEventCause(validationService.checkExistingEventCause(row, 1, 8));
+		events.setFailureClass(validationService.checkExistingFailureClass(row, 2));
+		events.setUeType(validationService.checkExistingUserEquipmentType(row, 3));
+		events.setMarketOperator(validationService.checkExistingMarketOperator(row, 4,5));
+	}
+
+	private void validateNonDatabaseDependentFields(Row row, Events events) {
+		events.setDateTime(validationService.checkDate(row, 0));
+		events.setCellId(validationService.checkCellId(row, 6));
+		events.setDuration(validationService.checkDuration(row, 7));
+		events.setNeVersion(validationService.checkNEVersion(row, 9));
+		events.setImsi(validationService.checkIMSI(row, 10));
+		events.setHier3Id(validationService.checkhier3Id(row, 11));
+		events.setHier32Id(validationService.checkhier32Id(row, 12));
 	}
 
 }
