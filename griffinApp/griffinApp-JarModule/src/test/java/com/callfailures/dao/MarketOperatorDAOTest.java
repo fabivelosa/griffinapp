04dc6531de1d086a66b6075e290acd343abb0a4f
Monisha Mani <a00279057@student.ait.ie>
2021-02-28 10:05:53 +0000

    Integration test for MarketOperatorDAO and entitymanager

---
+++ griffinApp/griffinApp-JarModule/src/test/java/com/callfailures/dao/MarketOperatorDAOTest.java
@@ -0,0 +1,52 @@
+package com.callfailures.dao;
+
+import static org.junit.Assert.*;
+import javax.inject.Inject;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ArchivePaths;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import com.callfailures.entity.EventCause;
+import com.callfailures.entity.EventCausePK;
+import com.callfailures.entity.Events;
+import com.callfailures.entity.FailureClass;
+import com.callfailures.entity.MarketOperator;
+import com.callfailures.entity.MarketOperatorPK;
+import com.callfailures.entity.UserEquipment;
+import com.callfailures.utils.test.EntityGenerator;
+
+@RunWith(Arquillian.class)
+public class MarketOperatorDAOTest {
+	   @Deployment
+	   public static JavaArchive createTestArchive() {
+	      return ShrinkWrap.create(JavaArchive.class, "test.jar")
+	         .addClasses(MarketOperatorDAO.class, Events.class, EventCause.class, 
+	        		 MarketOperator.class, UserEquipment.class, FailureClass.class,
+	        		 EventCausePK.class, MarketOperatorPK.class, EntityGenerator.class)
+	         .addAsManifestResource(
+	            "arquillian-persistence/persistence.xml", 
+	            ArchivePaths.create("persistence.xml"));
+	   }
+
+
+	   @Inject
+	   private MarketOperatorDAO marketOperatorDAO;
+	   
+	   private final EntityGenerator entityGenerator = new EntityGenerator();
+
+	   @Test
+	   public void testCanPersistMarketOperatorObject() {
+		   final int mCc = 238;
+		   final int mNc = 1;
+		   final String country = "Denmark";
+		   final String operator = "TDC-DK";
+		   final MarketOperator marketOperator = entityGenerator.parseMobileCountryNetworkCode(mCc, mNc, country, operator);
+		   marketOperatorDAO.create(marketOperator);
+		   final MarketOperator queryResult = marketOperatorDAO.getMarketOperator(new MarketOperatorPK(mCc, mNc));
+		   assertEquals(country, queryResult.getCountryDesc());
+	   }
+
+	}
