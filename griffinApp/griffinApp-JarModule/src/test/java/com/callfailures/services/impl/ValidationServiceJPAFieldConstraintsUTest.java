b843dd0351ad3424269883a0c9615838edb76e09
Wilmir <A00278899@student.ait.ie>
2021-02-28 10:57:49 +0000

    Renamed EventsServiceImplTest and Used the EntityGenerator Util Class

--- griffinApp/griffinApp-JarModule/src/test/java/com/callfailures/services/impl/ValidationServiceJPAFieldConstraintsUTest.java
+++ griffinApp/griffinApp-JarModule/src/test/java/com/callfailures/services/impl/ValidationServiceJPAFieldConstraintsUTest.java
@@ -21,9 +21,11 @@ import com.callfailures.entity.MarketOperatorPK;
 import com.callfailures.entity.UserEquipment;
 import com.callfailures.exception.FieldNotValidException;
 import com.callfailures.services.ValidationService;
+import com.callfailures.utils.test.EntityGenerator;
 
 public class ValidationServiceJPAFieldConstraintsUTest {
 
+	private EntityGenerator eventsGenerator = new EntityGenerator();
 	private ValidationService validationService;
 	private Validator validator;
 	
@@ -43,15 +45,13 @@ public class ValidationServiceJPAFieldConstraintsUTest {
 								final String eventCauseDescription, final String failureClassDescription,
 								final String country, final String operator) {
 		
-		final Events events = generateCallFailureInstance(localDateTime, eventId, failureId, ueId, mCc, mNc,
+		final Events events = eventsGenerator.generateCallFailureInstance(localDateTime, eventId, failureId, ueId, mCc, mNc,
 				cellId, duration, causeCode, neVersion, IMSI, hier3Id, hier32Id, hier321Id, eventCauseDescription,
 				failureClassDescription, country, operator);
 		
 		validationService.validate(events);
 		
 	}
-	
-	
 	
 	@ParameterizedTest
 	@CsvFileSource(resources = "/validationservice/invalidData.csv", numLinesToSkip = 1)
@@ -63,63 +63,13 @@ public class ValidationServiceJPAFieldConstraintsUTest {
 								final String country, final String operator) {
 		
 		
-		final Events callFailure = generateCallFailureInstance(localDateTime, eventId, failureId, ueId, mCc, mNc,
+		final Events callFailure = eventsGenerator.generateCallFailureInstance(localDateTime, eventId, failureId, ueId, mCc, mNc,
 				cellId, duration, causeCode, neVersion, IMSI, hier3Id, hier32Id, hier321Id, eventCauseDescription,
 				failureClassDescription, country, operator);
 				
 		final FieldNotValidException exception = assertThrows(FieldNotValidException.class, () -> validationService.validate(callFailure));
 		
 		assertEquals(invalidFieldName, exception.getInvalidFieldName());
-	}
-
-	private Events generateCallFailureInstance(final String localDateTime, final Integer eventId,
-			final Integer failureId, final Integer ueId, final Integer mCc, final Integer mNc, final Integer cellId,
-			final Integer duration, final Integer causeCode, final String neVersion, final String IMSI,
-			final String hier3Id, final String hier32Id, final String hier321Id, final String eventCauseDescription,
-			final String failureClassDescription, final String country, final String operator) {
-		final LocalDateTime dateTime = parseDateTime(localDateTime);
-		final EventCause eventCause = parseEventCause(eventId, causeCode, eventCauseDescription);
-		final FailureClass failureClass = parseFailureClass(failureId, failureClassDescription);
-		final MarketOperator marketOperator = parseMobileCountryNetworkCode(mCc, mNc, country, operator);
-		final UserEquipment ueType = parseUE(ueId);
-		
-		final Events events = new Events(eventCause, dateTime, failureClass, ueType, marketOperator, cellId, duration, neVersion,
-				IMSI, hier3Id, hier32Id, hier321Id);
-		
-		return events;
-	}
-
-	
-	private UserEquipment parseUE(final Integer ueId) {
-		final UserEquipment userEquipment = ueId == null ? null : new UserEquipment(ueId);
-		return userEquipment;
-	}
-
-	
-	private MarketOperator parseMobileCountryNetworkCode(final Integer mCc, final Integer mNc, final String country,
-			final String operator) {
-		final MarketOperator mobileCountryNetworkCode = new MarketOperator(new MarketOperatorPK(mCc, mNc), country, operator);
-		return mobileCountryNetworkCode;
-	}
-
-	
-	private FailureClass parseFailureClass(final Integer failureId, final String failureClassDescription) {
-		final FailureClass failureClass = new FailureClass(failureId, failureClassDescription);
-		return failureClass;
-	}
-
-	
-	private EventCause parseEventCause(final Integer eventId, final Integer causeCode,
-			final String eventCauseDescription) {
-		final EventCause eventCause = (eventId == null || causeCode == null) ? null : new EventCause(new EventCausePK(eventId, causeCode), eventCauseDescription);
-		return eventCause;
-	}
-
-	
-	private LocalDateTime parseDateTime(final String localDateTime) {
-		final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm"); 
-		final LocalDateTime dateTime = localDateTime == null ? null : LocalDateTime.parse(localDateTime, formatter);
-		return dateTime;
 	}
 
 }
