84f85811d930b7f2596dd047953e900c5ff401eb
Wilmir <A00278899@student.ait.ie>
2021-02-28 08:29:08 +0000

    Adds the Read and Persist Events Unit Test

--- griffinApp/griffinApp-JarModule/src/test/java/com/callfailures/services/impl/ReadandPersistEventsUTest.java
+++ griffinApp/griffinApp-JarModule/src/test/java/com/callfailures/services/impl/ReadandPersistEventsUTest.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.when;
 
 import java.io.File;
 import java.nio.file.Paths;
+import java.util.Iterator;
 
 import javax.validation.Validation;
 import javax.validation.Validator;
@@ -25,15 +26,17 @@ import com.callfailures.dao.MarketOperatorDAO;
 import com.callfailures.dao.UserEquipmentDAO;
 import com.callfailures.entity.EventCause;
 import com.callfailures.entity.EventCausePK;
+import com.callfailures.entity.Events;
 import com.callfailures.entity.FailureClass;
 import com.callfailures.entity.MarketOperator;
 import com.callfailures.entity.MarketOperatorPK;
 import com.callfailures.entity.UserEquipment;
 import com.callfailures.exception.FieldNotValidException;
+import com.callfailures.parsingutils.InvalidRow;
+import com.callfailures.parsingutils.ParsingResponse;
 import com.callfailures.services.EventService;
 import com.callfailures.services.ValidationService;
 
-@Disabled
 class ReadandPersisteEventsUTest {
 	private final EventCauseDAO eventCauseDAO = mock(EventCauseDAO.class);
 	private final FailureClassDAO failureClassDAO = mock(FailureClassDAO.class);
@@ -69,7 +72,6 @@ class ReadandPersisteEventsUTest {
 	}
 	
 	@Test
-	@Disabled
 	void testSuccessfuleReadOfFile() {
 		when(eventCauseDAO.getEventCause(anyObject())).thenReturn(eventCause);
 		when(failureClassDAO.getFailureClass(anyInt())).thenReturn(failureClass);
@@ -77,164 +79,130 @@ class ReadandPersisteEventsUTest {
 		when(marketOperatorDAO.getMarketOperator(marketOperatorPK)).thenReturn(marketOperator);
 		file = new File(absolutePath + "/importData/validData.xlsx");
 		eventService.read(file);
+		ParsingResponse<Events> parsingResults = eventService.read(file);
+		assertEquals(4, parsingResults.getValidObjects().size());
+		assertEquals(0, parsingResults.getInvalidRows().size());
 	}
 	
 	@Test
-	@Disabled
-	void testFailedValidationService() {
-		when(eventCauseDAO.getEventCause(anyObject())).thenReturn(eventCause);
-		when(failureClassDAO.getFailureClass(anyInt())).thenReturn(failureClass);
-		when(userEquipmentDAO.getUserEquipment(21060800)).thenReturn(userEquipment);
-		when(marketOperatorDAO.getMarketOperator(marketOperatorPK)).thenReturn(marketOperator);
-		file = new File(absolutePath + "/importData/validData.xlsx");
-		doThrow(new FieldNotValidException("imsi","Invalid IMSI")).when(validationService).validate(anyObject());
-		Throwable exception = assertThrows(FieldNotValidException.class, () -> eventService.read(file));
-		assertEquals("Invalid IMSI", exception.getMessage());
-	}
-	
-	@Test
-	@Disabled
 	void testInvalidDate() {
 		file = new File(absolutePath + "/importData/invalidDate.xlsx");
-		Throwable exception =  assertThrows(FieldNotValidException.class, () -> eventService.read(file));
-		assertEquals("Invalid Date", exception.getMessage());
+		assertInvalidRowMessage("Invalid Date");
 	}
 	
 	@Test
-	@Disabled
 	void testInvalidCellID() {
 		file = new File(absolutePath + "/importData/invalidCellId.xlsx");
-		Throwable exception =  assertThrows(FieldNotValidException.class, () -> eventService.read(file));
-		assertEquals("Invalid Cell ID", exception.getMessage());
+		assertInvalidRowMessage("Invalid Cell ID");
 	}
 	
 	@Test
-	@Disabled
 	void testInvalidDuration() {
 		file = new File(absolutePath + "/importData/invalidDuration.xlsx");
-		Throwable exception =  assertThrows(FieldNotValidException.class, () -> eventService.read(file));
-		assertEquals("Invalid Duration", exception.getMessage());
+		assertInvalidRowMessage("Invalid Duration");
 	}
 	
 	@Test
-	@Disabled
 	void testInvalidNEVersion() {
 		file = new File(absolutePath + "/importData/invalidNEVersion.xlsx");
-		Throwable exception =  assertThrows(FieldNotValidException.class, () -> eventService.read(file));
-		assertEquals("Invalid NE Version", exception.getMessage());
+		assertInvalidRowMessage("Invalid NE Version");
 	}
 	
 	@Test
-	@Disabled
 	void testInvalidIMSI() {
 		file = new File(absolutePath + "/importData/invalidIMSI.xlsx");
-		Throwable exception =  assertThrows(FieldNotValidException.class, () -> eventService.read(file));
-		assertEquals("Invalid IMSI", exception.getMessage());
+		assertInvalidRowMessage("Invalid IMSI");
 	}
 
 	@Test
-	@Disabled
 	void testInvalidHIER3_ID() {
 		file = new File(absolutePath + "/importData/invalidhier3Id.xlsx");
-		Throwable exception =  assertThrows(FieldNotValidException.class, () -> eventService.read(file));
-		assertEquals("Invalid hier3Id", exception.getMessage());
+		assertInvalidRowMessage("Invalid hier3Id");
 	}
 	
 	@Test
-	@Disabled
 	void testInvalidHIER32_ID() {
 		file = new File(absolutePath + "/importData/invalidhier32Id.xlsx");
-		Throwable exception =  assertThrows(FieldNotValidException.class, () -> eventService.read(file));
-		assertEquals("Invalid hier32Id", exception.getMessage());
+		assertInvalidRowMessage("Invalid hier32Id");
 	}
 	
 	@Test
-	@Disabled
 	void testInvalidHIER321_ID() {
 		file = new File(absolutePath + "/importData/invalidhier321Id.xlsx");
-		Throwable exception =  assertThrows(FieldNotValidException.class, () -> eventService.read(file));
-		assertEquals("Invalid hier321Id", exception.getMessage());
+		assertInvalidRowMessage("Invalid hier321Id");
 	}
 	
 	
 	@Test
-	@Disabled
 	void testInvalidEventCause() {
 		file = new File(absolutePath + "/importData/invalidEventCause.xlsx");
-		Throwable exception =  assertThrows(FieldNotValidException.class, () -> eventService.read(file));
-		assertEquals("Invalid Event and Cause Code combination", exception.getMessage());
+		assertInvalidRowMessage("Invalid Event and Cause Code combination");
 	}
 	
 
 	@Test
-	@Disabled
 	void testInexistentEventCause() {
 		file = new File(absolutePath + "/importData/inexistentEventCause.xlsx");
-		Throwable exception =  assertThrows(FieldNotValidException.class, () -> eventService.read(file));
-		assertEquals("Inexistent Event and Cause Code combination", exception.getMessage());
+		assertInvalidRowMessage("Inexistent Event and Cause Code combination");
 	}
 	
 	
 	@Test
-	@Disabled
 	void testInvalidFailureClass() {
 		when(eventCauseDAO.getEventCause(anyObject())).thenReturn(eventCause);
 		file = new File(absolutePath + "/importData/invalidFailureClass.xlsx");
-		Throwable exception =  assertThrows(FieldNotValidException.class, () -> eventService.read(file));
-		assertEquals("Invalid Failure Class Id", exception.getMessage());
+		assertInvalidRowMessage("Invalid Failure Class Id");
 	}
 	
 	@Test
-	@Disabled
 	void testInexistentFailureClass() {
 		when(eventCauseDAO.getEventCause(anyObject())).thenReturn(eventCause);
 		file = new File(absolutePath + "/importData/inexistentFailureClass.xlsx");
-		Throwable exception =  assertThrows(FieldNotValidException.class, () -> eventService.read(file));
-		assertEquals("Inexistent Failure Class Id", exception.getMessage());
+		assertInvalidRowMessage("Inexistent Failure Class Id");
 	}
 	
 	
 	@Test
-	@Disabled
 	void testInvalidUEType() {
 		when(eventCauseDAO.getEventCause(anyObject())).thenReturn(eventCause);
 		when(failureClassDAO.getFailureClass(anyInt())).thenReturn(failureClass);
 		file = new File(absolutePath + "/importData/invalidFailureClass.xlsx");
-		Throwable exception =  assertThrows(FieldNotValidException.class, () -> eventService.read(file));
-		assertEquals("Invalid Failure Class Id", exception.getMessage());
+		assertInvalidRowMessage("Invalid Failure Class Id");
 	}
 	
 	@Test
-	@Disabled
 	void testInexistentUEType() {
 		when(eventCauseDAO.getEventCause(anyObject())).thenReturn(eventCause);
 		when(failureClassDAO.getFailureClass(anyInt())).thenReturn(failureClass);
 		file = new File(absolutePath + "/importData/inexistentUEType.xlsx");
-		Throwable exception =  assertThrows(FieldNotValidException.class, () -> eventService.read(file));
-		assertEquals("Inexistent UE type", exception.getMessage());
+		assertInvalidRowMessage("Inexistent UE type");
 	}
 	
 	
 	@Test
-	@Disabled
 	void testInvalidMarketOperator() {
 		when(eventCauseDAO.getEventCause(anyObject())).thenReturn(eventCause);
 		when(failureClassDAO.getFailureClass(anyInt())).thenReturn(failureClass);
 		when(userEquipmentDAO.getUserEquipment(21060800)).thenReturn(userEquipment);
 		file = new File(absolutePath + "/importData/invalidMarketOperator.xlsx");
-		Throwable exception =  assertThrows(FieldNotValidException.class, () -> eventService.read(file));
-		assertEquals("Invalid  MCC and MNC combination", exception.getMessage());
+		assertInvalidRowMessage("Invalid  MCC and MNC combination");
 	}
 	
 	
 	@Test
-	@Disabled
 	void testInexistentMarketOperator() {
 		when(eventCauseDAO.getEventCause(anyObject())).thenReturn(eventCause);
 		when(failureClassDAO.getFailureClass(anyInt())).thenReturn(failureClass);
 		when(userEquipmentDAO.getUserEquipment(21060800)).thenReturn(userEquipment);
 		file = new File(absolutePath + "/importData/inexistentMarketOperator.xlsx");
-		Throwable exception =  assertThrows(FieldNotValidException.class, () -> eventService.read(file));
-		assertEquals("Inexistent MCC and MNC combination", exception.getMessage());
+		assertInvalidRowMessage("Inexistent MCC and MNC combination");
+	}
+	
+	private void assertInvalidRowMessage(String invalidRowMessage) {
+		ParsingResponse<Events> parsingResults = eventService.read(file);
+		assertEquals(0, parsingResults.getValidObjects().size());
+		assertEquals(1, parsingResults.getInvalidRows().size());
+		Iterator<InvalidRow> eventsIterator = parsingResults.getInvalidRows().iterator();
+		assertEquals(invalidRowMessage, eventsIterator.next().getErrorMessage());
 	}
 }
