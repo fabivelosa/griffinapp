faff13646e4e8a28f671327f9c3abb4e87c8e70c
Peter Casey <A00278938@student.ait.ie>
2021-03-03 15:47:14 +0000

    Erroneous Data displayed post upload

--- griffinApp/griffinApp-WarModule/src/main/webapp/js/uploadData.js
+++ griffinApp/griffinApp-WarModule/src/main/webapp/js/uploadData.js
@@ -4,33 +4,73 @@ var verifyLoaded=function(){
 	console.log('initiated');		
 };
 
-//Display all issues
-var getErrors=function(){
-	$.ajax({
-		   type:'GET',
-		   url: 'http://localhost:8080/callfailures/api/events/sampleJSON2',
-		   dataType:"json",
-		   success: displayErrors,
-		   error: function(){
-			console.log('Error occured');
-			}
-		   });
-};
-
 var displayErrors=function(data){
 	console.log('display Called');
 	$.each(data,function(index,DataErrorMessage){
-	$('#errorsList').append('<li>'+DataErrorMessage.rowNumber +'  '+DataErrorMessage.message+'</li>');
-				
+	$('#errorsList')
+	.append('<li>'+DataErrorMessage.rowNumber +'  '+DataErrorMessage.message+'</li>');
 			});
-}
+};
 
+//Add dataset
+var submitdata = function(){
+	console.log('submit called"');
+	var formData = new FormData();
+	var formData = new FormData($('#fileUploadForm')[0]); 
+	var files = $('#uploadFile')[0].files;
+
+	
+	if(files.length >0){
+		console.log('file found');
+	$.ajax({
+		type: 'POST',
+		url: 'http://localhost:8080/callfailures/api/file/upload',
+		dataType: "json",
+		data:  formData,
+		contentType:false,
+		processData: false,
+		success: function(data){
+			var errorCount = 0;
+			//Display Erronous Data
+			$.each(data,function(index,EventsUploadResponseDTO){
+				$.each(EventsUploadResponseDTO.erroneousData,function(index,InvalidRow){
+						$('#errorsList')
+						.append('<li> Error at Row'+InvalidRow.rowNumber + ', Cause of Error: '+ InvalidRow.errorMessage  +'</li>');
+						errorCount++;
+						});
+			});
+			
+			$('#displayCount').val('Errors Found: '+ errorCount);
+			
+		},
+		error: function(){
+			console.log('error');
+			$('#displayCount').val('fail');
+			//display error message here
+		}		
+		
+	});
+	}else{
+		console.log('Please select file');
+	}
+};
+
+
+var renderErrors = function(data){
+				$.each(data,function(){
+					$('#errorsList').append('')
+				});
+}
 
 $(document).ready(function(){
 	//Assign button to call function on click
-	$('#displayData').click(function(){
-		getErrors();
-	});
+	//$('#uploadBtn').click(function(){submitdata();});
 		
+	$('#fileUploadForm').submit(function(event){ 
+ 	event.preventDefault();
+ 	console.log('Clicked upload');
+ 	submitdata(); 
+	});
+	
 	verifyLoaded();
 });
