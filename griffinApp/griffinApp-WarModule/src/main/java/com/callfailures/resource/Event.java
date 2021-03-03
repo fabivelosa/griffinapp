6435487bb0c393fa45b92c9dd436d46470a1fce1
fabivelosa <fabivelosa@gmail.com>
2021-03-03 07:28:37 +0000

    clear pmd validations

--- griffinApp/griffinApp-WarModule/src/main/java/com/callfailures/resource/Event.java
+++ griffinApp/griffinApp-WarModule/src/main/java/com/callfailures/resource/Event.java
@@ -9,22 +9,22 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.callfailures.entity.Events;
-import com.callfailures.services.EventService; 
+import com.callfailures.services.EventService;
 
-@Path("/events") 
+@Path("/events")
 @Stateless
 public class Event {
 
 	@EJB
 	private EventService eventService;
-	
+
 	@GET
 	@Path("/{id}")
 	@Produces({ MediaType.APPLICATION_JSON })
-	public Events getEvent(@PathParam("id") int eventId) {
-		Events event = eventService.findById(eventId);
+	public Events getEvent(@PathParam("id") final int eventId) {
+		final Events event = eventService.findById(eventId);
 		System.out.println(event.getEventCause().getDescription());
 		return eventService.findById(eventId);
-	} 
-	
+	}
+
 }
