e35c610b7c3c7012ecfebd67af60a02fd55914d8
Wilmir <A00278899@student.ait.ie>
2021-03-09 15:11:58 +0000

    Adds Error Messages DTO and Enum

---
+++ griffinApp/griffinApp-WarModule/src/main/java/com/callfailures/errors/ErrorMessages.java
@@ -0,0 +1,15 @@
+package com.callfailures.errors;
+
+public class ErrorMessages {
+	private final String errorMessage;
+
+	public ErrorMessages(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	
+	
+}
