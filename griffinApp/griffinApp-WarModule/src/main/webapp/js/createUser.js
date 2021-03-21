bbc78142f37ea8270ab266979e8b176bb2464acf
Dan <a00279030@student.ait.ie>
2021-03-21 18:43:21 +0000

    addition of login link to index page

--- griffinApp/griffinApp-WarModule/src/main/webapp/js/createUser.js
+++ griffinApp/griffinApp-WarModule/src/main/webapp/js/createUser.js
@@ -1,3 +1,23 @@
-/**
- * 
- */
+var createUser = function(){
+  console.log('create user called"');
+	var formData = new FormData();
+	var formData = new FormData($('#createUserForm'));
+  var enter = $('.entry');
+
+  if(entry.length > 0){
+    console.log('Valid input');
+    
+  }
+}
+
+$(document).ready(function(){
+	//Assign button to call function on click
+	//$('#uploadBtn').click(function(){submitdata();});
+
+	$('#createUserForm').create(function(user){
+
+ 	console.log('Clicked create user');
+ 	createUser();
+	});
+
+});
