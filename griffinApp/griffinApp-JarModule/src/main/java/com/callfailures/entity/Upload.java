5e3664f84875830b311963a25e24c4e519be1936
Monisha Mani <a00279057@student.ait.ie>
2021-03-22 01:55:29 +0000

    Ìntroduce upload table to track the upload status

---
+++ griffinApp/griffinApp-JarModule/src/main/java/com/callfailures/entity/Upload.java
@@ -0,0 +1,45 @@
+package com.callfailures.entity;
+
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.hibernate.annotations.Type;
+
+@Entity(name = "upload")
+@XmlRootElement
+@Table(name = "upload")
+public class Upload {
+	@Id
+	@Type(type="org.hibernate.type.UUIDCharType")
+	private UUID uploadID;
+
+	private String uploadStatus;
+	
+	public UUID getUploadID() {
+		return uploadID;
+	}
+
+	public void setUploadID(UUID uploadID) {
+		this.uploadID = uploadID;
+	}
+
+	public String getUploadStatus() {
+		return uploadStatus;
+	}
+
+	public void setUploadStatus(String uploadStatus) {
+		this.uploadStatus = uploadStatus;
+	}
+
+	public Upload() {
+	}
+
+	}
