90d7026689a73988c1f8ecdaeffd88da5c135fc4
Dan <a00279030@student.ait.ie>
2021-03-28 18:56:35 +0100

    Html cleaned up (One query on page) password validation added (required characters) dropdown for userType added css added (serving validation functions)

--- griffinApp/griffinApp-WarModule/src/main/webapp/js/newUser.js
+++ griffinApp/griffinApp-WarModule/src/main/webapp/js/newUser.js
@@ -1,4 +1,5 @@
 var rootURL = 'http://localhost:8080/callfailures/api';
+var authToken = 'Bearer '+sessionStorage.getItem("auth-token");
 
 var currentUser;
 
@@ -7,6 +8,17 @@ var newUser=function () {
 	currentUser = {};
 	renderDetails(currentUser); // Display empty form
 };
+
+var pass = document.getElementById('userPassword');
+
+pass.onfocus = function() {
+  	document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+pass.onblur = function() {
+ 	document.getElementById("message").style.display = "none";
+}
 
 var addUser = function () {
 	console.log('addUser');
@@ -18,6 +30,9 @@ var addUser = function () {
 		dataType: 'json',
 		data: formToJSON(),
 		contentType: 'application/json',
+		beforeSend: function(xhr){
+		xhr.setRequestHeader('Authorization', authToken);
+		},
 		success: function(data, textStatus, jqXHR){
 			alert('User created successfully');
 			
@@ -42,6 +57,48 @@ var renderDetails=function(users){
 	
 }
 
+
+
+
+pass.onkeyup = function() {
+  
+  var lowerCaseLetters = /[a-z]/g;
+  if(pass.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+
+  var upperCaseLetters = /[A-Z]/g;
+  if(pass.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  var numbers = /[0-9]/g;
+  if(pass.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  var length = document.getElementById("length");
+  if(pass.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
+
 var formToJSON=function () {
 	
 	return JSON.stringify({
@@ -58,10 +115,16 @@ $(document).ready(function(){
 	
 	
 	$('#createUserButton').click(function() {
-		if ($('#userPassword').val() == $('#confirmPassword').val())
+		if (($('#userId').val() == "")||($('#userName').val() == "")||($('#userType').val() == "")||($('#userPassword').val() == "")){
+			alert('Required field left empty');
+			}
+			
+		else if ($('#userPassword').val() == $('#confirmPassword').val())
 			addUser();
+			
 		else
 			alert('Error: Passwords must match');
+			
 		return false;
 	});
 	
