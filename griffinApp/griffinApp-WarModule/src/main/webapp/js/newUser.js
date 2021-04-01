1836fa09e6725da614e2d4e74e1fb04529cf62d7
Dan <a00279030@student.ait.ie>
2021-04-01 16:00:28 +0100

    adding alerts for successful user creation, missing required field, non-matching passwords

--- griffinApp/griffinApp-WarModule/src/main/webapp/js/newUser.js
+++ griffinApp/griffinApp-WarModule/src/main/webapp/js/newUser.js
@@ -9,6 +9,17 @@ var newUser=function () {
 	renderDetails(currentUser); // Display empty form
 };
 
+var pass = document.getElementById('userPassword');
+
+pass.onfocus = function() {
+  	document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+pass.onblur = function() {
+ 	document.getElementById("message").style.display = "none";
+}
+
 var addUser = function () {
 	console.log('addUser');
 	console.log(formToJSON());
@@ -23,7 +34,7 @@ var addUser = function () {
 		xhr.setRequestHeader('Authorization', authToken);
 		},
 		success: function(data, textStatus, jqXHR){
-			alert('User created successfully');
+			document.getElementById("successMessage").style.display = "block";
 			
 			$('#userId').val("");
 			$('#userName').val("");
@@ -46,6 +57,48 @@ var renderDetails=function(users){
 	
 }
 
+
+
+
+pass.onkeyup = function() {
+  
+  var lowerCaseLetters = /[a-z]/g;
+  if(pass.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+
+  var upperCaseLetters = /[A-Z]/g;
+  if(pass.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  var numbers = /[0-9]/g;
+  if(pass.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  var length = document.getElementById("length");
+  if(pass.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
+
 var formToJSON=function () {
 	
 	return JSON.stringify({
@@ -57,16 +110,29 @@ var formToJSON=function () {
 		});
 };
 
+document.getElementById("successMessage").style.display= "none";
+document.getElementById("alertMissingField").style.display= "none";
+document.getElementById("alertPasswordMatch").style.display= "none";
+
+var clear = document.getElementById("createUserButton");
+
+clear.onblur=function(){
+	document.getElementById("successMessage").style.display= "none";
+	document.getElementById("alertMissingField").style.display= "none";
+	document.getElementById("alertPasswordMatch").style.display= "none";	
+}
+
 $(document).ready(function(){
 	$('#createUserButton').click(function() {
 		if (($('#userId').val() == "")||($('#userName').val() == "")||($('#userType').val() == "")||($('#userPassword').val() == "")){
-			alert('Required field left empty');
-			}
+		document.getElementById("alertMissingField").style.display= "block";
+		}
+		else if($('#userPassword').val() != $('#confirmPassword').val())
+					document.getElementById("alertPasswordMatch").style.display= "block";			
 			
-		else if ($('#userPassword').val() == $('#confirmPassword').val())
-			addUser();
-		else
-			alert('Error: Passwords must match');
+		else{
+			document.getElementById("successMessage").style.display= "block";	
+			addUser();}
 		return false;
 	});
 	
