763fe505edc798b77e8859fea36946dde240b343
Wilmir <A00278899@student.ait.ie>
2021-04-01 12:49:06 +0100

    Removes unnecessary properties for line chart

--- griffinApp/griffinApp-WarModule/src/main/webapp/js/networkEngineer.js
+++ griffinApp/griffinApp-WarModule/src/main/webapp/js/networkEngineer.js
@@ -87,8 +87,6 @@ const displayIMSISummaryChart = function(imsiSummary){
         backgroundColor: "#4e73df",
         hoverBackgroundColor: "#2e59d9",
         borderColor: "#4e73df",
-        barPercentage:0.5,
-        categoryPercentage:1.0,
         data: [imsiSummary.totalDurationMs/1000],
         type:"line"
       }
