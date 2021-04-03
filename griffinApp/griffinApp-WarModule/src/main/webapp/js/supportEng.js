95cbc594891917ca6906400e0701e952e54b8eac
Simon Harper <A00239007@student.ait.ie>
2021-04-03 19:49:17 +0100

    Chart added

--- griffinApp/griffinApp-WarModule/src/main/webapp/js/supportEng.js
+++ griffinApp/griffinApp-WarModule/src/main/webapp/js/supportEng.js
@@ -61,7 +61,8 @@ const queryCallFailureCount = function(imsi, from, to){
         success: function(response){
             displayCallFailureCount(response);
             const endTime = new Date().getTime();
-            displayResponseSummary(response, startTime, endTime);
+            //displayResponseSummary(response, startTime, endTime);
+            displayIMSISummaryChart(response);
         },
         error: displayErrorOnIMSISummary2
     })
@@ -82,6 +83,92 @@ const autoCompleteIMSI2 = function(){
         }
     })
 }
+
+const displayIMSISummaryChart = function(imsiSummary){
+    $("#imsiSummaryFormResultChartCard").show();
+    var ctx = $("#imsiSummaryFormResultChart")[0];
+    const imsiSummaryChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: [imsiSummary.model],
+        datasets: [{
+          label: "Total Count",
+          backgroundColor: "#4e73df",
+          hoverBackgroundColor: "#4e73df",
+          borderColor: "#4e73df",
+          barPercentage:0.5,
+          categoryPercentage:1.0,
+          maxBarThickness:200,
+          data: [imsiSummary.callFailuresCount],
+        }],
+      },
+      options: {
+        maintainAspectRatio: false,
+        layout: {
+          padding: {
+            left: 10,
+            right: 25,
+            top: 25,
+            bottom: 0
+          }
+        },
+        scales: {
+          xAxes: [{
+            type:"category",
+            gridLines: {
+              display: false,
+              drawBorder: false
+            },
+            ticks: {
+              maxTicksLimit: 1
+            }
+          }],
+          yAxes: [{
+            ticks: {
+              min: 0,
+              max: Math.ceil((imsiSummary.callFailuresCount)),       
+              padding: 10,
+            },
+            scaleLabel: {
+              display: true,
+              labelString: 'Call Failures Count'
+            },
+            gridLines: {
+              color: "rgb(234, 236, 244)",
+              zeroLineColor: "rgb(234, 236, 244)",
+              drawBorder: false,
+              borderDash: [2],
+              zeroLineBorderDash: [2]
+            }
+          }],
+        },
+        legend: {
+          display: true,
+          position:'bottom'
+        },
+        tooltips: {
+          titleMarginBottom: 10,
+          titleFontColor: '#6e707e',
+          titleFontSize: 14,
+          backgroundColor: "rgb(255,255,255)",
+          bodyFontColor: "#858796",
+          borderColor: '#dddfeb',
+          borderWidth: 1,
+          xPadding: 15,
+          yPadding: 15,
+          displayColors: false, 
+          caretPadding: 10,
+          callbacks: {
+            label: function(tooltipItem, chart) {
+              var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+              return datasetLabel + ': ' + tooltipItem.yLabel;
+            }
+          }
+        },
+      }
+    });
+    $("#imsiSummaryFormResultDate").text(`Data from ${$('#startDateOnCallFailureForm').val()} to ${$('#endDateOnCallFailureForm').val()}`);
+  }
 
 //Display
 const displayIMSIAffectedByFailureClass = function(IMSIs){
