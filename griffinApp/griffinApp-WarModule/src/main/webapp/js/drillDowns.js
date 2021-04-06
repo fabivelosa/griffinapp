6012b7241b065e086ae805ce20f8e6bf2ed93f59
Wilmir <A00278899@student.ait.ie>
2021-04-06 11:11:07 +0100

    Adds the drillDown.js file

--- griffinApp/griffinApp-WarModule/src/main/webapp/js/drillDowns.js
+++ griffinApp/griffinApp-WarModule/src/main/webapp/js/drillDowns.js
@@ -1,5 +1,5 @@
 const drillDownInit = function(){
-    console.log("hello");
+    console.log("Drill down is initialized");
 }
 
 const topTenIMSIDrillDown = function(event, array){
@@ -7,22 +7,5 @@ const topTenIMSIDrillDown = function(event, array){
     if(activeBar[0]){
         console.log(activeBar);
     }
-
-    // if (activePoints[0]) {
-    //   console.log(activePoints.length);
-    //   console.log(activePoints[0]);
-
-    //   var chartData = activePoints[0]['_chart'].config.data;
-    //   var idx = activePoints[0]['_index'];
-
-    //   var label = chartData.labels[idx];
-    //   var value = chartData.datasets[0].data[idx];
-    //   var color = chartData.datasets[0].backgroundColor[idx]; //Or any other data you wish to take from the clicked slice
-
-    //   console.log(chartData.datasets[0]);
-
-
-    //   alert(label + ' ' + value + ' ' + color);
-    // }
 };
 
