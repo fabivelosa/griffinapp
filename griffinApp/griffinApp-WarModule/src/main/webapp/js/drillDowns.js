790f44d58ab4e100242af19adfe92541068c2d99
Wilmir <A00278899@student.ait.ie>
2021-04-06 23:41:23 +0100

    Calls the backend when the bar is clicked

--- griffinApp/griffinApp-WarModule/src/main/webapp/js/drillDowns.js
+++ griffinApp/griffinApp-WarModule/src/main/webapp/js/drillDowns.js
@@ -2,22 +2,40 @@ const drillDownInit = function(){
     console.log("Drill down is initialized");
 }
 
+const hideAllSections = function(){
+    $.each($("#queryNESelectors").children(), function(index, selector) {
+        $(`#${$(selector).data("section")}`).hide();
+    });
+    $.each($("#querySESelectors").children(), function(index, selector) {
+        $(`#${$(selector).data("section")}`).hide();
+    });
+    $.each($("#queryCSSelectors").children(), function(index, selector) {
+        $(`#${$(selector).data("section")}`).hide();
+    });
+}
+
+
+const displayListOfIMSIEventForDrillDown = function(imsiEventsList){
+    hideAllSections();
+    $("#networkEngQueryFourDrillDown").show();
+    console.log(imsiEventsList);
+}
+
 const queryListOfIMSIEventForDrillDown = function(imsi, fromTime, toTime){
     console.log(`${imsi} ${fromTime} ${toTime}`);
     $.ajax({
         type:'GET',
         dataType:'json',
-        url:`${rootURL2}/IMSIs/query?imsi=${imsi}&from=${fromTim}&to=${toTims}`,
+        url:`${rootURL2}/IMSIs/query?imsi=${imsi}&from=${fromTime}&to=${toTime}`,
         beforeSend: setAuthHeader1,
-        success: function(response){
-            console.log(response);
+        success: function(imsiEventsList){
+            displayListOfIMSIEventForDrillDown(imsiEventsList);
         },
         error: function(jqXHR, textStatus, errorThrown){
             console.log("There is an error in queryListOfIMSIEventForDrillDown")
         }
     });
 }
-
 
 const topTenIMSIDrillDown = function(event, array){
     let activeBar = this.getElementAtEvent(event);
