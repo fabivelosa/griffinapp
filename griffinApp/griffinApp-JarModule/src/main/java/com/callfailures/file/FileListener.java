a72b1436b40c0258a3e9abe8b67c460bd492699d
fabivelosa <fabivelosa@gmail.com>
2021-04-06 16:20:47 +0100

    clear pmd violations.

--- griffinApp/griffinApp-JarModule/src/main/java/com/callfailures/file/FileListener.java
+++ griffinApp/griffinApp-JarModule/src/main/java/com/callfailures/file/FileListener.java
@@ -8,20 +8,14 @@ import java.nio.file.Paths;
 import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
-import java.nio.file.WatchService;
-import java.util.UUID;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 
-import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import com.callfailures.dao.UploadDAO;
 import com.callfailures.entity.Upload;
 import com.callfailures.services.FileService;
 
@@ -31,9 +25,6 @@ public class FileListener {
 
 	@Inject
 	FileService fileService;
-
-	@Inject
-	private UploadDAO uploadDAO;
 
 	@PostConstruct
 	public void init() {
@@ -50,27 +41,22 @@ public class FileListener {
 		new Thread() {
 			public void run() {
 				try {
-					WatchService watchService = FileSystems.getDefault().newWatchService();
 					// Change directory to server directory
-					Path directory = Paths.get(System.getProperty("user.dir") + "/fileUploads/");
-					WatchKey watchKey = directory.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
-
+					final Path directory = Paths.get(System.getProperty("user.dir") + "/fileUploads/");
+					final WatchKey watchKey = directory.register(FileSystems.getDefault().newWatchService(),
+							StandardWatchEventKinds.ENTRY_CREATE);
 					while (true) {
-						for (WatchEvent<?> event : watchKey.pollEvents()) {
-
-							Path filePath = directory.resolve((Path) event.context());
+						for (final WatchEvent<?> event : watchKey.pollEvents()) {
+							final Path filePath = directory.resolve((Path) event.context());
 							final String fileName = filePath.toFile().getName();
 							final int dotIndex = fileName.lastIndexOf('.');
 							final String fileEnd = fileName.substring(dotIndex);
-							if (fileEnd.equals(".xlsx")) {
+							if (".xlsx".equals(fileEnd)) {
 								System.out.println("Processing: " + filePath.getFileName());
 								System.out.println("Path : " + filePath);
-								File file = new File(filePath.toString());
-								Workbook workbook = new XSSFWorkbook(file);
-
-								Upload currentUpload = fileService.createUploadFile();
-
-								fileService.processFile(workbook,currentUpload);
+								final File file = new File(filePath.toString());
+								final Upload currentUpload = fileService.createUploadFile();
+								fileService.processFile(new XSSFWorkbook(file), currentUpload);
 							}
 						}
 					}
@@ -81,6 +67,4 @@ public class FileListener {
 			}
 		}.start();
 	}
-
-	
 }
