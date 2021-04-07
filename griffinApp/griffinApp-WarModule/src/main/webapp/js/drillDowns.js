2b8beb08e95195fce894d719ed1fbc1470ff872d
Wilmir <A00278899@student.ait.ie>
2021-04-07 13:15:24 +0100

    Adds the drill down table and line chart

--- griffinApp/griffinApp-WarModule/src/main/webapp/js/drillDowns.js
+++ griffinApp/griffinApp-WarModule/src/main/webapp/js/drillDowns.js
@@ -1,6 +1,35 @@
-const drillDownInit = function(){
-    console.log("Drill down is initialized");
+/**
+ * @author Wilmir Nicanor
+ * @email wbnicanor01@gmail.com
+ */
+
+
+
+/* Util Methods for the Graph*/
+const getRoundedUpYAxisMaxValue = function(chartData){
+    const maxValue = Math.max(...chartData.map(data => data.y));
+    return Math.pow(10, Math.ceil(Math.log10(maxValue)));
 }
+
+const generateIncrementalTimeSeriesData = function(dataset){
+    const countPerTimeMap = dataset.reduce((map,event) => {
+        let key = new Date(event.dateTime).toISOString();
+        if(key in map){
+            map[key] += 1;
+        }else{
+            map[key] = 1;
+        }        
+        return map;
+    }, {});
+    const timeSeriesData = [];
+    for(key in countPerTimeMap){
+        timeSeriesData.push({
+            x:key,
+            y: countPerTimeMap[key]
+        })
+    };
+    return timeSeriesData;
+};
 
 const hideAllSections = function(){
     $.each($("#queryNESelectors").children(), function(index, selector) {
@@ -14,15 +43,89 @@ const hideAllSections = function(){
     });
 }
 
+/* Line Chart Default Config*/
+const imsiLineChartConfig = {
+    type: "line",
+    data: {
+      labels: [],
+      datasets: [{
+        data: [],
+        label: "Call Failures",
+        borderColor: "#3e95cd",
+        fill: false
+      }]
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          type: "time",
+          distribution: "linear"
+        }],
+        yAxes: [{
+        ticks: {
+          min: 0,
+          max: 100,     
+          padding: 10,
+        },
+        scaleLabel: {
+          display: true,
+          labelString: 'Call Failures Count/Duration'
+        },
+        gridLines: {
+          color: "rgb(234, 236, 244)",
+          zeroLineColor: "rgb(234, 236, 244)",
+          drawBorder: false,
+          borderDash: [2],
+          zeroLineBorderDash: [2]
+        }
+      }],
+        title: {
+          display: false
+        }
+      },
+      maintainAspectRatio: false,
+      layout: {
+        padding: {
+          left: 10,
+          right: 25,
+          top: 25,
+          bottom: 0
+        }
+      }
+    }
+};
+
 
 const displayListOfIMSIEventForDrillDown = function(imsiEventsList){
     hideAllSections();
     $("#networkEngQueryFourDrillDown").show();
-    console.log(imsiEventsList);
+    displayNetworkEngQueryFourDrillDownTable(imsiEventsList);
+    displayNetworkEngQueryFourDrillDownChart(imsiEventsList);
+}
+
+const displayNetworkEngQueryFourDrillDownTable = function(imsiEventsList){
+    const table = $('#networkEngQueryFourDrillDownTable').DataTable();
+    table.clear();
+    $(imsiEventsList).each(function(index, event){
+        table.row.add([event.dateTime, 
+            event.imsi,
+            event.eventCause.description
+        ]);
+    });
+    table.draw();
+}
+
+const displayNetworkEngQueryFourDrillDownChart = function(imsiEventsList){
+    const context = $("#networkEngQueryFourDrillDownChart")[0];
+    const networkEngQueryFourDrillDownChart = new Chart(context, imsiLineChartConfig);
+    let chartData = generateIncrementalTimeSeriesData(imsiEventsList);
+    networkEngQueryFourDrillDownChart.data.datasets[0].data = chartData;
+    imsiLineChartConfig.options.scales.yAxes[0].ticks.max = getRoundedUpYAxisMaxValue(chartData);
+    networkEngQueryFourDrillDownChart.update();
 }
 
 const queryListOfIMSIEventForDrillDown = function(imsi, fromTime, toTime){
-    console.log(`${imsi} ${fromTime} ${toTime}`);
+    const startTime = new Date();
     $.ajax({
         type:'GET',
         dataType:'json',
@@ -30,6 +133,9 @@ const queryListOfIMSIEventForDrillDown = function(imsi, fromTime, toTime){
         beforeSend: setAuthHeader1,
         success: function(imsiEventsList){
             displayListOfIMSIEventForDrillDown(imsiEventsList);
+            const endTime = new Date();
+            displayResponseSummary(imsiEventsList, startTime, endTime);
+            console.log(imsiLineChartConfig);
         },
         error: function(jqXHR, textStatus, errorThrown){
             console.log("There is an error in queryListOfIMSIEventForDrillDown")
@@ -37,7 +143,7 @@ const queryListOfIMSIEventForDrillDown = function(imsi, fromTime, toTime){
     });
 }
 
-const topTenIMSIDrillDown = function(event, array){
+const topTenIMSIDrillDownEventHandler = function(event, array){
     let activeBar = this.getElementAtEvent(event);
     if(activeBar[0]){
         let index = activeBar[0]["_index"];
@@ -47,4 +153,3 @@ const topTenIMSIDrillDown = function(event, array){
         queryListOfIMSIEventForDrillDown(imsi, fromTime, toTime);
     }
 };
-
